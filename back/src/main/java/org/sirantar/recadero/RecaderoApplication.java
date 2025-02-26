d018454c0e0de76a441cf29df661cf4eb80914f1
Salvador Rioboo Naranjo <salrio74@gmail.com>
2025-02-26 08:56:44 +0100

    feat: add backend base

---
+++ back/src/main/java/org/sirantar/recadero/RecaderoApplication.java
@@ -0,0 +1,13 @@
+package org.sirantar.recadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecaderoApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/org/sirantar/recadero/RecaderoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sirantar.recadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
